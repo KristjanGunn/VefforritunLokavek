7491f282093bcf9c9d726b5f2fb3bb922431981e
Snorri <vaskur_@hotmail.com>
2014-11-28 18:40:01 +0000

    dót

---
+++ Verkefni_Loka/index.js
@@ -0,0 +1,32 @@
+
+
+document.addEventListener('DOMContentLoaded',function()
+{ 
+    // Adds hide/show on click to the following elements
+    showHide("storyOne", "ferrantiMark");
+    showHide("storyTwo","williamHiginBotham");
+    showHide("storyThree","theBrownBox");
+    showHide("storyFour","atari");
+    showHide("storyFive","videoGameCrash");
+    showHide("storySix","consoleWar");
+    showHide("storySeven","pcGaming");
+    showHide("storyEight","today");
+});
+
+// A function to hide or show an element when
+// another element is clicked
+function showHide(clickedID, showID){
+    // The elements
+    var clickeddiv = document.getElementById(clickedID);
+    var item = document.getElementById(showID);
+    // Listen for the click
+    clickeddiv.addEventListener("click",function(){
+        // If the item is invisible
+        if(item.offsetParent === null){
+            // make it visible
+            item.style.display = 'initial';
+        }
+        // else hide it
+        else item.style.display = 'none';
+    });
+}
